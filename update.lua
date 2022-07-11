7b9cb34e24e956ca9dd47867088711a5ddbba344
Mr. Dorn <Dorn@DornDev.net>
2022-07-11 11:47:36 +0200

    Update update.lua

--- update.lua
+++ update.lua
@@ -11,10 +11,11 @@ term.clear()
 print("Updating...")
 print("[      ]")
 os.sleep(0.5)
+term.clear()
 print("Updating...")
 print("[II    ]")
 
-local files = fs.list() -- list of files in directory
+local files = fs.list(".") -- list of files in directory
 
 for i,v in pairs(files) do
     print(v)
